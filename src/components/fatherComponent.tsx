b345f3daad6b98eb62310c2a75b28d8004890cfc
chenp <chenpeleg@gmail.com>
2023-02-09 12:20:15 +0200

    renaming componenet

--- src/components/fatherComponent.tsx
+++ src/components/fatherComponent.tsx
@@ -1,13 +1,29 @@
-import {MyComponent} from "./myComponent";
+import {NasaImage} from "./nasaImage";
 import {useEffect, useState} from "react";
 
 const API_URL = 'https://images-api.nasa.gov/search?q='
-const DEFALT_SEARCH = 'webb'
+const DEFALT_SEARCH = 'webb';
+
+//TODO:
+// Part A - Make the app show one image (the first one) from the NASA Api
+//    With name and date
+// The way:
+// 1. build a typescript model (interface) in the models folder
+// 2. pass the interface to the child component
+// ........
+// ........
+// Part B - Make the app show multiple image (the first one) from the NASA Api
+// add keywords (an array in the api)
+// .......
+// api information : https://api.nasa.gov/#nasa-image-and-video-library
+// ...
+// The images are on the links with {href : string, render: 'image'}
+
 export const FatherComponent = (props: any) => {
     const [ searchResults, setSearchResults ] :any= useState([])
     useEffect(  () => {
        fetch(`${API_URL}${DEFALT_SEARCH}`).then(res=>(res.json().then(r=>  setSearchResults(r) )  ))
     });
     console.log(searchResults)
-    return (  <MyComponent></MyComponent>)
+    return (  <NasaImage></NasaImage>)
 }
--- src/components/nasaImage.tsx
+++ src/components/nasaImage.tsx
@@ -1,5 +1,6 @@
 
-export const MyComponent = (props: any) => {
+
+export const NasaImage = (props: any) => {
     const propsAsJson = JSON.stringify(props,null, "  ")
     return (  <div className={'json-display'}>
         <pre>{propsAsJson}</pre>
