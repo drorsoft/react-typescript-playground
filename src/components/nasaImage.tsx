a6807f0108ce0dbe197f1a34355e8cb2c3e8a767
chenp <chenpeleg@gmail.com>
2023-03-05 09:25:49 +0200

    added click handler

--- src/components/nasaImage.tsx
+++ src/components/nasaImage.tsx
@@ -1,13 +1,32 @@
 import {NasaImage} from "../models/NasaImage";
+import {Style} from "util";
+import React from "react";
+import {UserChoiceEnum} from "../models/userChoice.enum";
 
+const buttonStyle : React.CSSProperties = {
 
-export const NasaImageComponent = (props: NasaImage) => {
+}
+export interface NasaImageComponentProps {
+    image : NasaImage;
+    clickHandler : (choice:UserChoiceEnum) => void
+}
+
+export const NasaImageComponent = (props: NasaImageComponentProps) => {
+    const image = props.image;
+    const buttonClick = (choice:UserChoiceEnum) =>{
+        props.  clickHandler(choice)
+    }
+
     return (
+
         <div style={{display: 'flex', flexDirection : 'column'}}>
             <>
-                <h3>  {props.title}</h3>
-                <img src={props.imageUrl} alt={`${props.title}`}/>
-               <h4> {props.date.toISOString()}</h4>
+                <div id={'buttons-container'}>
+                    <button style={{...buttonStyle}}></button>
+                </div>
+                <h3>  {image.title}</h3>
+                <img src={image.imageUrl} alt={`${image.title}`}/>
+               <h4> {image.date.toISOString()}</h4>
             </>
         </div>
 
---
+++ src/models/userChoice.enum.ts
@@ -0,0 +1,6 @@
+
+export enum UserChoiceEnum  {
+   First = 1,
+    Last = 2,
+    Random = 3
+}
