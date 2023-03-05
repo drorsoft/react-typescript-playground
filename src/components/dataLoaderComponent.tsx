2a67ba3f1d86d1319c902e392dd5f83f58af885d
chenp <chenpeleg@gmail.com>
2023-03-05 09:14:33 +0200

    added solution 1

--- src/components/dataLoaderComponent.tsx
+++ src/components/dataLoaderComponent.tsx
@@ -24,21 +24,19 @@ interface NasaSearchResults {
 
 
 
-//TODO:
-// Part A - Make the app show one image (the first one) from the NASA Api
-//    With title and date
-// The way:
-// 1. build a typescript model for (interface) for the props passed to NasaImage
-// 2. (optional) build a typescript model for (interface) for data from the api
-// 3. pass the interface to the child component
-// ........
-// ........
-// Part B - Make the app show multiple image (the first one) from the NASA Api
-// add keywords (an array in the api)
-// .......
-// api information : https://api.nasa.gov/#nasa-image-and-video-library
-// ...
-// The images are on the links with {href : string, render: 'image'}
+
+//TODO (Exercise 2):
+// Part A:
+// Add 3 buttons to the nasaImage component, that request different images:
+// 1. The first image
+// 2. The last image
+// 3. A random image
+// -----------
+// Part B:
+// Build a Generic function that takes an object with a (date : Date) property,
+// clones it and returns the object with a formatted date property
+
+
 
 const searchResultsToImage = (searchResults: NasaSearchResults): NasaImage => {
     const firstItem: Item = searchResults.collection.items[0];
@@ -54,7 +52,7 @@ export const DataLoaderComponent = (props: any) => {
     const [searchResults, setSearchResults] = useState<NasaSearchResults>()
     useEffect(() => {
         fetch(`${API_URL}${DEFALT_SEARCH}`).then(res => (res.json().then(r => setSearchResults(r))))
-    });
+    },[]);
 
 
     return (searchResults ? <NasaImageComponent {...searchResultsToImage(searchResults)}></NasaImageComponent> : null)
