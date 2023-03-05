ebf4531ea60eb09e8439214c35c055d3062a490e
chenp <chenpeleg@gmail.com>
2023-03-05 09:34:14 +0200

    added button styles

--- src/components/nasaImage.tsx
+++ src/components/nasaImage.tsx
@@ -2,33 +2,41 @@ import {NasaImage} from "../models/NasaImage";
 import React from "react";
 import {UserChoiceEnum} from "../models/userChoice.enum";
 
-const buttonStyle : React.CSSProperties = {
-
+const buttonStyle: React.CSSProperties = {
+    all: 'unset',
+    margin: '20px',
+    padding: '8px',
+    fontSize : '18px',
+    cursor: 'pointer',
+    background : '#fd0083',
+    color: 'black',
+    borderRadius : '10px'
 }
+
 export interface NasaImageComponentProps {
-    image : NasaImage;
-    clickHandler : (choice:UserChoiceEnum) => void
+    image: NasaImage;
+    clickHandler: (choice: UserChoiceEnum) => void
 }
 
 export const NasaImageComponent = (props: NasaImageComponentProps) => {
     const image = props.image;
-    const buttonClick = (choice:UserChoiceEnum) =>{
+    const buttonClick = (choice: UserChoiceEnum) => {
         props.clickHandler(choice)
     }
 
     return (
 
-        <div style={{display: 'flex', flexDirection : 'column'}}>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
             <>
-                <div id={'buttons-container'}>
-                    <button style={{...buttonStyle}} onClick={()=>buttonClick(UserChoiceEnum.First)}>First</button>
-                    <button style={{...buttonStyle}} onClick={()=>buttonClick(UserChoiceEnum.Random)}>Random</button>
-                    <button style={{...buttonStyle}} onClick={()=>buttonClick(UserChoiceEnum.Last)}>Last</button>
+                <div id={'buttons-container'} style={{display: 'flex', justifyContent: 'space-around'}}>
+                    <button style={{...buttonStyle}} onClick={() => buttonClick(UserChoiceEnum.First)}>First</button>
+                    <button style={{...buttonStyle}} onClick={() => buttonClick(UserChoiceEnum.Random)}>Random</button>
+                    <button style={{...buttonStyle}} onClick={() => buttonClick(UserChoiceEnum.Last)}>Last</button>
 
                 </div>
                 <h3>  {image.title}</h3>
                 <img src={image.imageUrl} alt={`${image.title}`}/>
-               <h4> {image.date.toISOString()}</h4>
+                <h4> {image.date.toISOString()}</h4>
             </>
         </div>
 
